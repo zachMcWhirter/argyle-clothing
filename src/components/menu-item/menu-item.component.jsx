3caa5a697094f6dd2903782227a4ae55b7747e47
Zach McWhirter <zmac7811@gmail.com>
2021-03-05 11:49:28 -0600

    add background images and set up js template strings for scss styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,11 +4,18 @@ import "./menu-item.styles.scss"
 
     // Destructure the {title} property from props and
     // pass it into the h1 so it works dynamically with all menu-items
-const MenuItem = ({ title }) => (
-  <div className="menu-item">
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div 
+    style={{
+          // using a js template strings for: 
+          // `url(${imageUrl})`  and  {`${size} "menu-item"`}
+      backgroundImage: `url(${imageUrl})`
+    }} 
+    className={`${size} menu-item`}
+  >
     <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
