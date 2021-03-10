205e8ad33fcb91f0b8188924eb514b937301e37d
Zach McWhirter <zmac7811@gmail.com>
2021-03-10 07:30:29 -0600

    setup combined page for signin and signup. and setup routing to it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Header from "./components/header/header-component";
+
 import './App.css';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+
+import Header from "./components/header/header-component";
 
 
 function App() {
@@ -14,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+  // Use a functional component here because we want the state to live at 
+  // a lower level - (in the sign-in  &  sign-up components respectively)
+const SignInAndSignUp = () => (
+  <div className="sign-in-and-sign-up">
+    SIGN IN
+  </div>
+);
+
+export default SignInAndSignUp;
