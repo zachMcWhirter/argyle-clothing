2829e6ac05b06de5cda1cdf7bcad3400a8954210
Zach McWhirter <zmac7811@gmail.com>
2021-03-11 12:46:35 -0600

    get and set userRef

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header-component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 
 class App extends React.Component {
@@ -22,10 +22,10 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-
-      console.log(user);
+    auth.onAuthStateChanged(async user => {
+      // this.setState({ currentUser: user });
+      createUserProfileDocument(user);
+      // console.log(user);
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,35 @@ const config = {
   messagingSenderId: "110459262015",
   appId: "1:110459262015:web:aeeb73dc95e40b1462148a",
   measurementId: "G-NJ1QHLFEGK"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  
+    // userRef will store the value of the document reference
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    // then we use the stored value of userRef to access the .get() method 
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+
 }
 
 firebase.initializeApp(config);
